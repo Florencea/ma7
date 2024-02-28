8fbe677c41ffd0d2456eef37205750e67275c4db
Florencea <bearflorencea@gmail.com>
2024-02-29 01:45:53 +0800

    refactor: optimize speed

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,19 @@
+import { LocalizedStringProvider } from "@adobe/react-spectrum/i18n";
 import type { Metadata } from "next";
 import type { ReactNode } from "react";
 import "tailwindcss/tailwind.css";
 
 export const metadata: Metadata = {
-  title: "MA7",
   description: "MA7",
 };
 
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="zh-TW">
-      <body id="__next">{children}</body>
+      <body id="__next">
+        <LocalizedStringProvider locale="zh-TW" />
+        {children}
+      </body>
     </html>
   );
 }
