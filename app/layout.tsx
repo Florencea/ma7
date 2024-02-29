9e56af842f7de77f5a5cf5fa96a9abc611b388de
Florencea <bearflorencea@gmail.com>
2024-02-29 14:48:15 +0800

    refactor: add inter font

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,13 @@
 import { LocalizedStringProvider } from "@adobe/react-spectrum/i18n";
 import type { Metadata } from "next";
+import { Inter } from "next/font/google";
 import type { ReactNode } from "react";
 import "./global.css";
+
+const inter = Inter({
+  subsets: ["latin"],
+  display: "swap",
+});
 
 export const metadata: Metadata = {
   title: "MA7",
@@ -10,7 +16,7 @@ export const metadata: Metadata = {
 
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
-    <html lang="zh-TW">
+    <html lang="zh-TW" className={inter.className}>
       <body id="__next">
         <LocalizedStringProvider locale="zh-TW" />
         {children}
