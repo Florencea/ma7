9e3652d5ad7bd2116abb810c2363fe97c5401cdc
Florencea <bearflorencea@gmail.com>
2024-02-29 15:15:11 +0800

    revert: use spectrum design principle

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import { LocalizedStringProvider } from "@adobe/react-spectrum/i18n";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import type { ReactNode } from "react";
-import "./global.css";
+import "tailwindcss/tailwind.css";
 
 const inter = Inter({
   subsets: ["latin"],
