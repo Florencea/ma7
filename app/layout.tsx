d831885c99b71450e1c5fcb2f6caa2203976e37d
Florencea <bearflorencea@gmail.com>
2024-03-02 01:42:17 +0800

    fix: remove inter font

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,7 @@
 import { LocalizedStringProvider } from "@adobe/react-spectrum/i18n";
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
 import type { ReactNode } from "react";
 import "tailwindcss/tailwind.css";
-
-const inter = Inter({
-  subsets: ["latin"],
-  display: "swap",
-});
 
 export const metadata: Metadata = {
   title: "MA7",
@@ -16,7 +10,7 @@ export const metadata: Metadata = {
 
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
-    <html lang="zh-TW" className={inter.className}>
+    <html lang="zh-TW">
       <body id="__next">
         <LocalizedStringProvider locale="zh-TW" />
         {children}
